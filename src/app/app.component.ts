cea0d714687cebf65e80acb4d4d5cdbf482e2514
Lucas Borgonovi <lucas.borgonovi@skf.com>
2021-08-10 19:14:24 -0300

    First Module complete

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // o seletor será uma tag utilizada no html dentro do index.html
+  selector: 'app-root',
+  // aqui estamos definindo o arquivo html desse componente
+  templateUrl: './app.component.html',
+  // Aqui define-se o arquivo css do componente
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  
+  title = 'course-manager';
+
+  name: string = "Lucas";
+
+
+}
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+})
+// OnInit executa uma ação, ao inicializar o componente
+export class CourseListComponent implements OnInit{
+
+    filteredCourses: Course[] = [];
+
+    _courses: Course[] = [];
+
+    _filterBy: string = '';
+
+    constructor(private courseService: CourseService){
+
+    }
+
+    ngOnInit(): void{
+
+        this._courses = this.courseService.retrieveAll();
+  
+
+    }
+
+    set filter(value:string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course:Course)=>course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter(){
+        return this._filterBy;
+    }
+
+}
